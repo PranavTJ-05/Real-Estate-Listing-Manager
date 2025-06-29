3ab157381d51a4977126a53f76e1c13d9ff28bd4
HarshitaDua07 <154653264+HarshitaDua07@users.noreply.github.com>
2025-06-29 12:42:22 +0530

    Add files via upload

--- eslint.config.js
+++ eslint.config.js
@@ -1,78 +1,29 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import react from 'eslint-plugin-react';
-import prettier from 'eslint-plugin-prettier';
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+  { ignores: ["dist"] },
   {
-    ignores: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
-  },
-  // Configuration for JavaScript files (including this config file)
-  {
-    files: ['**/*.{js,jsx}'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      parserOptions: {
-        ecmaFeatures: { jsx: true },
-      },
+      ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      prettier,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
-      'react/react-in-jsx-scope': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
-      'prettier/prettier': 'error',
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  },
-  // Configuration for TypeScript files
-  {
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        sourceType: 'module',
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        project: ['./tsconfig.app.json', './tsconfig.node.json'],
-      },
-      globals: globals.browser,
-    },
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      prettier,
-    },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      'prettier/prettier': 'error',
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
+      "@typescript-eslint/no-unused-vars": "off",
     },
   }
 );
